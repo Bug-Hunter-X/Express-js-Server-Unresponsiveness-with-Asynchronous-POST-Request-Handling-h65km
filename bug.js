973b1269c928c1830b459dc19c40c398b29a7442
BugHunter-X <askzenai@gmail.com>
2024-12-26 12:06:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+app.post('/data', (req, res) => {
+  const data = req.body;
+  // Process the data asynchronously
+  setTimeout(() => {
+    console.log('Processing data:', data);
+    res.send('Data received');
+  }, 5000); // Simulate a 5-second delay
+});
+app.listen(3000, () => console.log('Server listening on port 3000'));
